a1f17ced76543f99e5c1a8393a5f6a22cb874844
jtomacheski12 <140642624+jtomacheski12@users.noreply.github.com>
2025-09-15 09:44:12 -0300

    sdfghj

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const numeroSenha = document.querySelector('parametro-senha-texto');
+const numeroSenha = document.querySelector('.parametro-senha-texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 const checkbox = document.querySelectorAll('.checkbox');
@@ -53,11 +53,11 @@ function classificaSenha(tamanhoAlfabeto){
     forcaSenha.classList.remove('fraca', 'media', 'forte');
     if (entropia > 57){
         forcaSenha.classList.add('forte');
-    } else if (entropia > 35 && entropia < 57)(
-        forcaSenha.classList.add ('media');
-    ) else if (entropia < 35){
+    } else if (entropia > 35 && entropia < 57){
+        forcaSenha.classList.add('media');
+    } else if (entropia < 35){
         forcaSenha.classList.add ('fraca');
     }
     const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = "Um computador pode levar até"  + Math.floor(2**entropia/(100e6*60*60*24)) + "dias para descobrir essa senha";
+    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha";
 }
